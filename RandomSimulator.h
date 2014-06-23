fdec28024a0fb5f7556e820af5568bd8fb80f2db
rustyBilges <tophermcw@gmail.com>
2014-06-23 12:58:27 +0100

    Added file: RandomSimulator.h, class to generate randomised parameters with stable dynamics

---
+++ RandomSimulator.h
@@ -0,0 +1,63 @@
+/*
+ * randomSimulator.h
+ *
+ *  Created on: 23 Jun 2014
+ *      Author: rusty
+ */
+
+#ifndef RANDOMSIMULATOR_H_
+#define RANDOMSIMULATOR_H_
+
+#include <string.h>
+using namespace std;
+
+class RandomSimulator{
+
+private:
+	int simID, unitCount, timestepCount;
+	double dt;
+	string modelType, responseType;
+	unsigned int seed;
+
+	Simulation* sim;
+	IUnitUpdater* updater;
+
+public:
+	RandomSimulator(int simID, int unitCount, int timestepCount, double dt, string modelType, string responseType, unsigned int seed=0):simID(simID),unitCount(unitCount), timestepCount(timestepCount), dt(dt), modelType(modelType), responseType(responseType), seed(seed){
+
+		sim = new Simulation(simID, unitCount, timestepCount, dt, modelType, responseType);
+		updater = new EulerUnitUpdater();
+	}
+
+	bool runUntilStable(){
+		srand(seed);
+		bool finish = false;
+
+		Simulation* sim;
+
+		while (!finish){
+
+			sim = new Simulation(simID, unitCount, timestepCount, dt, modelType, responseType);
+			if (!sim->run(updater)){
+				delete sim;
+			}
+			else {
+//				cout << "simulation run" << endl;
+				finish = true;
+			}
+		}
+
+		return true;
+	}
+
+	bool save(string saveSuffix=""){
+		sim->save(saveSuffix);
+		return true;
+	}
+
+
+
+};
+
+
+#endif /* RANDOMSIMULATOR_H_ */
